77c89acb6c0c06d86743bf72121b640507eba457
mthsilva98 <mthsilva1998@gmail.com>
2025-03-26 15:21:49 -0300

    Implementa o Scraper para buscar links de PDFs

---
+++ src/main/java/com/intuitivecare/webscraping/service/Scraper.java
@@ -0,0 +1,29 @@
+package com.intuitivecare.webscraping.service;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Scraper {
+    private static final String URL_BASE = "https://www.gov.br/ans/pt-br/acesso-a-informacao/participacao-da-sociedade/atualizacao-do-rol-de-procedimentos";
+
+    public static List<String> getPdflinks() throws IOException {
+        List<String> pdfLinks = new ArrayList<>();
+        Document doc = Jsoup.connect(URL_BASE).get();
+        Elements links = doc.select("a[href$=.pdf]");
+
+        for (Element link : links) {
+            String fileUrl = link.absUrl("href");
+            if((fileUrl.contains("Anexo-I") || fileUrl.contains("Anexo-II"))) {
+                pdfLinks.add(fileUrl);
+            }
+        }
+        return pdfLinks;
+    }
+
+}
